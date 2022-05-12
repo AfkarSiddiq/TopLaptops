bd47bf3ce6c6e51b6af943c53731a49ceba21256
Amirul Ahsan <amirul_a@mhs.unsyiah.ac.id>
2022-05-12 11:45:12 +0700

    gambar

--- TopLaptops/routes/web.php
+++ TopLaptops/routes/web.php
@@ -13,12 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/home', function () {
     return view('home');
 });
 
-<<<<<<< Updated upstream
-Route::get('/index', function () {
+
+Route::get('/', function () {
     return view('template');
 });
 
@@ -26,5 +26,3 @@ Route::get('/kuliah', function(){
     return view ('laptopKuliah');
 });
 
-=======
->>>>>>> Stashed changes
